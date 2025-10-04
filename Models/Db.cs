5f092c5dd9eeb8a47fa84010067ff2158a0b46e0
Johan Janérs <johanjaners@gmail.com>
2025-10-04 17:22:39 +0200

    Added database class Db, new developer Id 1+ 2

---
+++ Models/Db.cs
@@ -0,0 +1,14 @@
+using webapiDotNetTrainingGround.Controllers;
+using webapiDotNetTrainingGround.Models;
+
+public class Db
+{
+    public Db()
+    {
+        Developers = new List<Developer>() {
+            new Developer() { Id = 1, Name = "Marcus", Email = "marcus@salt.dev"},
+            new Developer() { Id = 2, Name = "Beatrice", Email = "bea@salt.dev"},
+        };
+    }
+    public List<Developer> Developers { get; set; }
+}
