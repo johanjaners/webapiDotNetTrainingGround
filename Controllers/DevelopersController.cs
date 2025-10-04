b2d7e37a2e860acb35b2521a5ca9af2c2977d729
Johan Janérs <johanjaners@gmail.com>
2025-10-04 17:56:35 +0200

    DevelopersConstrollers.cs: update private field _db  and constructor using Db, Program.cs: builder.Services <Db>

--- Controllers/DevelopersController.cs
+++ Controllers/DevelopersController.cs
@@ -6,20 +6,25 @@ namespace webapiDotNetTrainingGround.Controllers;
 [Route("/api/[controller]")]
 public class DevelopersController : ControllerBase
 {
-    private static readonly List<Developer> _db = [];
+    private readonly Db _db;
+
+    public DevelopersController()
+    {
+        _db = new Db();
+    }
 
     [HttpGet]
-    public List<Developer> GetAllDevelopers() => _db;
+    public List<Developer> GetAllDevelopers() => _db.Developers;
 
     [HttpGet("{id}")]
-    public Developer? GetDeveloperById(int id) => _db.Find(d => d.Id == id);
+    public Developer? GetDeveloperById(int id) => _db.Developers.Find(d => d.Id == id);
 
     [HttpPost]
     public IActionResult CreateNewDeveloper(Developer developerToAdd)
     {
-        var nextId = _db.Count + 1;
+        var nextId = _db.Developers.Count + 1;
         developerToAdd.Id = nextId;
-        _db.Add(developerToAdd);
+        _db.Developers.Add(developerToAdd);
 
         return CreatedAtAction(nameof(GetDeveloperById), new { id = nextId }, developerToAdd);
     }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<Db>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
